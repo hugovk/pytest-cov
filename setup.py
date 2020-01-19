d641f68745a0d37503102a6a82ff085eae2ccea2
Hugo <hugovk@users.noreply.github.com>
2020-01-19 13:03:04 +0200

    Drop support for EOL Python 2.7

--- setup.py
+++ setup.py
@@ -106,13 +106,12 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Testing',
@@ -125,7 +124,7 @@ setup(
         'pytest>=3.6',
         'coverage>=4.4'
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.5',
     extras_require={
         'testing': [
             'fields',
